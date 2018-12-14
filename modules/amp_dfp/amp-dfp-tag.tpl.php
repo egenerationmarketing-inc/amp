3f18597a4b297149f74ded336559601e7da2230e
marcelovani <marcelovani@854220.no-reply.drupal.org>
2018-12-14 11:57:12 +0000

    Issue #3016664 by marcelovani, BigEd: Support sticky ads

--- modules/amp_dfp/amp-dfp-tag.tpl.php
+++ modules/amp_dfp/amp-dfp-tag.tpl.php
@@ -9,16 +9,21 @@
  * - width: The width of the ad.
  * - slot: The DFP ad slot string.
  * - amp_ad_json: Other settings, such as targeting, encoded in json.
+ * - sticky: If the tag is sticky.
  * - tag: The full Drupal DFP tag object
  *
  * @see template_preprocess_amp_ad()
  */
 ?>
+
+<?php if ($sticky): ?><amp-sticky-ad layout="nodisplay"><?php endif; ?>
+
 <amp-ad type="doubleclick"
-        layout="<?php print $layout; ?>"
+        <?php if ($layout): ?>layout="<?php print $layout; ?>"<?php endif; ?>
         height="<?php print $height; ?>"
         width="<?php print $width; ?>"
         data-slot="<?php print $slot; ?>"
         json='<?php print $amp_ad_json; ?>'
   >
 </amp-ad>
+<?php if ($sticky): ?></amp-sticky-ad><?php endif; ?>
